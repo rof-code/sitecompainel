9b695ded80d14e37bd6599d04b3c0e91eb543ee7
Rodolfo Augusto Rodrigues <62609956+rofbravls@users.noreply.github.com>
2020-09-14 20:21:17 -0300

    Add files via upload

---
+++ projeto_02 (1).sql
@@ -0,0 +1,250 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Tempo de geração: 15-Set-2020 às 00:39
+-- Versão do servidor: 10.4.11-MariaDB
+-- versão do PHP: 7.2.30
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Banco de dados: `projeto_02`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `tb_admin.online`
+--
+
+CREATE TABLE `tb_admin.online` (
+  `id` int(11) NOT NULL,
+  `ip` varchar(255) NOT NULL,
+  `ultima_acao` datetime NOT NULL,
+  `token` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `tb_admin.users`
+--
+
+CREATE TABLE `tb_admin.users` (
+  `id` int(11) NOT NULL,
+  `user` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `img` varchar(255) NOT NULL,
+  `nome` varchar(255) NOT NULL,
+  `cargo` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Extraindo dados da tabela `tb_admin.users`
+--
+
+INSERT INTO `tb_admin.users` (`id`, `user`, `password`, `img`, `nome`, `cargo`) VALUES
+(1, 'admin', 'admin', 'rof.jpg', 'Rodolfo Augusto Rodrigues', 2),
+(2, 'devcodardevelopment', 'BgUdBcA5i5GyPsS*QR', '5ea9ada6b8a7e.jpg', 'Alguem', 0);
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `tb_admin.visitas`
+--
+
+CREATE TABLE `tb_admin.visitas` (
+  `id` int(11) NOT NULL,
+  `ip` varchar(255) NOT NULL,
+  `dia` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Extraindo dados da tabela `tb_admin.visitas`
+--
+
+INSERT INTO `tb_admin.visitas` (`id`, `ip`, `dia`) VALUES
+(1, '::1', '2020-04-29'),
+(2, '::1', '2020-04-29'),
+(3, '::1', '2020-04-29'),
+(4, '::1', '2020-05-10'),
+(5, '::1', '2020-07-25'),
+(6, '::1', '2020-09-14');
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `tb_site.config`
+--
+
+CREATE TABLE `tb_site.config` (
+  `titulo` varchar(255) NOT NULL,
+  `nome_autor` varchar(255) NOT NULL,
+  `descricao` text NOT NULL,
+  `icone1` varchar(255) NOT NULL,
+  `descricao1` text NOT NULL,
+  `icone2` varchar(255) NOT NULL,
+  `descricao2` text NOT NULL,
+  `icone3` varchar(255) NOT NULL,
+  `descricao3` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `tb_site.depoimentos`
+--
+
+CREATE TABLE `tb_site.depoimentos` (
+  `id` int(11) NOT NULL,
+  `nome` varchar(255) NOT NULL,
+  `depoimento` text NOT NULL,
+  `data` varchar(255) NOT NULL,
+  `order_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Extraindo dados da tabela `tb_site.depoimentos`
+--
+
+INSERT INTO `tb_site.depoimentos` (`id`, `nome`, `depoimento`, `data`, `order_id`) VALUES
+(1, 'Raquel', 'Ótimo serviço! Obrigado a todos', '29/04/2020', 1),
+(4, 'Cassio ', 'Sistema show!', '02/05/2020', 0),
+(8, 'Hugo Ferreira', 'Muito bom trabalhar com vocês! Obrigado e parabéns pelo serviço!', '24/04/2020', 0),
+(9, 'Mirian', 'Top demais!', '08/06/2020', 2);
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `tb_site.servicos`
+--
+
+CREATE TABLE `tb_site.servicos` (
+  `id` int(11) NOT NULL,
+  `servico` text NOT NULL,
+  `order_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Extraindo dados da tabela `tb_site.servicos`
+--
+
+INSERT INTO `tb_site.servicos` (`id`, `servico`, `order_id`) VALUES
+(2, 'Criação de site LandingPage para Snop Vila Santana', 3),
+(3, 'hellooooooo', 1),
+(4, 'mais serviço aqui22', 2);
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `tb_site.slides`
+--
+
+CREATE TABLE `tb_site.slides` (
+  `id` int(11) NOT NULL,
+  `nome` varchar(255) NOT NULL,
+  `slide` varchar(255) NOT NULL,
+  `order_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Extraindo dados da tabela `tb_site.slides`
+--
+
+INSERT INTO `tb_site.slides` (`id`, `nome`, `slide`, `order_id`) VALUES
+(5, 'Cadastro 1 ', '5eaae266ac906.jpg', 0);
+
+--
+-- Índices para tabelas despejadas
+--
+
+--
+-- Índices para tabela `tb_admin.online`
+--
+ALTER TABLE `tb_admin.online`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Índices para tabela `tb_admin.users`
+--
+ALTER TABLE `tb_admin.users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Índices para tabela `tb_admin.visitas`
+--
+ALTER TABLE `tb_admin.visitas`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Índices para tabela `tb_site.depoimentos`
+--
+ALTER TABLE `tb_site.depoimentos`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Índices para tabela `tb_site.servicos`
+--
+ALTER TABLE `tb_site.servicos`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Índices para tabela `tb_site.slides`
+--
+ALTER TABLE `tb_site.slides`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT de tabelas despejadas
+--
+
+--
+-- AUTO_INCREMENT de tabela `tb_admin.online`
+--
+ALTER TABLE `tb_admin.online`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
+
+--
+-- AUTO_INCREMENT de tabela `tb_admin.users`
+--
+ALTER TABLE `tb_admin.users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT de tabela `tb_admin.visitas`
+--
+ALTER TABLE `tb_admin.visitas`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT de tabela `tb_site.depoimentos`
+--
+ALTER TABLE `tb_site.depoimentos`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+
+--
+-- AUTO_INCREMENT de tabela `tb_site.servicos`
+--
+ALTER TABLE `tb_site.servicos`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT de tabela `tb_site.slides`
+--
+ALTER TABLE `tb_site.slides`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
